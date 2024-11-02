390a34d3cb672c0deafe3ede34f77a482475e6ef
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-11-02 12:08:38 +0100

    Initial Commit

---
+++ News Feed UIKit CleanSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News Feed UIKit CleanSwift
+//
+//  Created by user246073 on 11/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
